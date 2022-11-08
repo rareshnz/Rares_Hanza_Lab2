4c2eefe57ff5c8d28f0ead688a839f3f294d0892
Rares Hanza <rares.hanza7@gmail.com>
2022-11-08 15:14:07 +0200

    Add project files.

---
+++ Rares_Hanza_Lab2/Data/Rares_Hanza_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rares_Hanza_Lab2.Models;
+
+namespace Rares_Hanza_Lab2.Data
+{
+    public class Rares_Hanza_Lab2Context : DbContext
+    {
+        public Rares_Hanza_Lab2Context (DbContextOptions<Rares_Hanza_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rares_Hanza_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Rares_Hanza_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Rares_Hanza_Lab2.Models.Author> Author { get; set; }
+    }
+}
