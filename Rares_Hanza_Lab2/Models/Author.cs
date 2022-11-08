914813fce8071f0d06fc43ff965954b77550c86a
Rares Hanza <rares.hanza7@gmail.com>
2022-11-08 16:27:41 +0200

    op

--- Rares_Hanza_Lab2/Models/Author.cs
+++ Rares_Hanza_Lab2/Models/Author.cs
@@ -1,10 +1,21 @@
-﻿namespace Rares_Hanza_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Rares_Hanza_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
