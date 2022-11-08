8067e9a3c26e8d7514fb01dde040a0f285124bb2
Rares Hanza <rares.hanza7@gmail.com>
2022-11-08 16:50:58 +0200

    yu

--- Rares_Hanza_Lab2/Pages/Books/Index.cshtml.cs
+++ Rares_Hanza_Lab2/Pages/Books/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Rares_Hanza_Lab2.Data;
 using Rares_Hanza_Lab2.Models;
@@ -23,9 +24,15 @@ namespace Rares_Hanza_Lab2.Pages.Books
         public BookData BookD { get; set; }
         public int BookID { get; set; }
         public int CategoryID { get; set; }
-        public async Task OnGetAsync(int? id, int? categoryID)
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder, string searchString)
         {
             BookD = new BookData();
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = String.IsNullOrEmpty(sortOrder) ? "author_desc" : "";
+            CurrentFilter = searchString;
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
             .Include(b => b.BookCategories)
@@ -33,12 +40,29 @@ namespace Rares_Hanza_Lab2.Pages.Books
             .AsNoTracking()
             .OrderBy(b => b.Title)
             .ToListAsync();
-            if (id != null)
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BookD.Books = BookD.Books.Where(s => s.Author.FirstName.Contains(searchString)
+                || s.Author.LastName.Contains(searchString)
+                || s.Title.Contains(searchString));
+            }
+                if (id != null)
             {
                 BookID = id.Value;
                 Book book = BookD.Books
                 .Where(i => i.ID == id.Value).Single();
                 BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                    s.Title);
+                    break;
+                case "author_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                    s.Author.FullName);
+                    break;
             }
         }
     }
